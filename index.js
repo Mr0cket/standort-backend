edc9ddb3d2470916e231f2d5dea6bde50bcda38d
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-01-26 14:21:57 +0100

    added login/signup flow with new endpoints
    new endpoints are /login, /signup, /me

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
-const cors = require("cors");
-const {PORT} = require("./config/constants");
+const loggerMiddleWare = require("morgan");
+const corsMiddleWare = require("cors");
+const { PORT } = require("./config/constants");
+const authRouter = require("./routers/auth");
+const authMiddleWare = require("./auth/middleware");
 
 const app = express();
 
-app.use(cors());
+app.use(loggerMiddleWare("dev"));
 
-app.get("/", (req, res) => {
-    res.send("Hi from express");
-});
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
 
+app.use(corsMiddleWare());
+
+if (process.env.DELAY) {
+  app.use((req, res, next) => {
+    setTimeout(() => next(), parseInt(process.env.DELAY));
+  });
+}
+
+app.use("/", authRouter);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
