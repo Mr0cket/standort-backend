fc2f4528d3d127af702d3e39703593d97e91b038
Milo Silva <milo.silva234@gmail.com>
2021-01-27 17:50:10 +0100

    Fix Integration of GET /posts/:location endpoint

--- routes/feedByLocation.js
+++ routes/feedByLocation.js
@@ -3,20 +3,24 @@ const Posts = require("../models").post;
 const Comments = require("../models").comment;
 const Users = require("../models").user;
 
-router.get('/posts/:location', async (req, res) => {
-        try {
-            const location = req.params.location;
+router.get("/posts/:location", async (req, res) => {
+  try {
+    const location = req.params.location;
 
-            const posts = await Posts.findAll({
-                where: {location: location},
-                include: {
-                    model: Users,
-                    required: true
-                }
-            });
-            const comments = await Comments.findAll({where: {postId: posts.map(post => post.id)}});
-            
-            let newPosts = [];
+    const posts = await Posts.findAll({
+      where: { location: location },
+      include: [
+        {
+          model: Users,
+          required: true,
+          attributes: ["name", "id", "profilePic", "homeLocation"],
+        },
+        Comments,
+      ],
+    });
+    // const comments = await Comments.findAll({where: {postId: posts.map(post => post.id)}});
+
+    /*             let newPosts = [];
 
             posts.forEach((post) => {
                 newPosts.push({
@@ -24,13 +28,12 @@ router.get('/posts/:location', async (req, res) => {
                     "comments": comments.filter(comment => comment.postId === post.id)
                 })
             });
-
-            res.send(newPosts);
-        } catch
-            (error) {
-            console.log(error.message);
-        }
-    }
-);
+ */
+    const parsedPosts = posts.map((post) => post.get({ plain: true }));
+    res.send(parsedPosts);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 module.exports = router;
