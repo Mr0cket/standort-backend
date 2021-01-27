3a49bfde2501b145b4d3497afee9dd92992d513c
Milo Silva <milo.silva234@gmail.com>
2021-01-27 10:16:59 +0100

    change endpoint to reflect the data currently sent

--- routes/posts.js
+++ routes/posts.js
@@ -6,13 +6,14 @@ const Post = require("../models").post;
 
 // POST /posts
 router.post("/", async (req, res, next) => {
-  // for later: picture, tags, votes, location
-  const { title, message } = req.body;
+  // for later: picture, votes, location
+  const { title, message, tags, userId = 1 } = req.body;
   try {
-    const newPost = await Post.create({ title, message });
-    res.send(newPost);
+    const newPost = await Post.create({ userId, title, message, tags });
+    res.send(newPost.dataValues);
   } catch (e) {
     console.log(`[posts]sequelize error: ${e.message}`);
+    next(e);
   }
 });
 
